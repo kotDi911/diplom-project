587d0023eeff78bbf601b983a36efb49874ba8ea
Nikolay <=>
2022-01-31 21:44:42 +0200

    v1.2.2

--- src/js/Theme.js
+++ src/js/Theme.js
@@ -3,33 +3,66 @@ export class Theme {
         if(!localStorage.getItem("theme")){
             localStorage.setItem("theme", "light");
         }
+//-----------v1
+        // if(localStorage.getItem("theme") === "light"){
+        //     Theme.lightTheme(container);
+        // }else {
+        //     Theme.darkTheme(container);
+        // }
+//-----------v1
 
-        this.curentTheme = localStorage.getItem("theme");
+//-----------v2
+        switch (localStorage.getItem("theme")) {
+            case "light":
+                Theme.lightTheme(container);
+                break;
+            case "dark":
+                Theme.darkTheme(container);
+                break;
+            case "red":
+                Theme.redTheme(container);
+                break;
 
-        if(this.curentTheme === "light"){
-            Theme.lightTheme(container);
-        }else {
-            Theme.darkTheme(container);
         }
     }
-    static switchTheme(container){
+//-----------v2
+    static switchTheme(container, select){
+        // if(localStorage.getItem("theme") === "light"){
+        //     Theme.darkTheme(container);
+        //     localStorage.setItem("theme", "dark")
+        // }else {
+        //     Theme.lightTheme(container);
+        //     localStorage.setItem("theme", "light")
+        // }
 
-        if(localStorage.getItem("theme") === "light"){
-            Theme.darkTheme(container);
-            localStorage.setItem("theme", "dark")
-        }else {
-            Theme.lightTheme(container);
-            localStorage.setItem("theme", "light")
+        switch (localStorage.setItem("theme", select)) {
+            case "light":
+                Theme.lightTheme(container);
+                localStorage.setItem("theme", "light");
+                break;
+            case "dark":
+                Theme.darkTheme(container);
+                localStorage.setItem("theme", "dark");
+                break;
+            case "red":
+                Theme.redTheme(container);
+                localStorage.setItem("theme", "red");
+                break;
+
         }
+        Theme.actualTheme(container)
     }
     static darkTheme(container){
-        container.classList.remove('light-theme');
+        container.classList.remove('light-theme', 'red-theme');
         container.classList.add('dark-theme');
     }
 
     static lightTheme(container){
-        container.classList.remove('dark-theme');
+        container.classList.remove('dark-theme', 'red-theme');
         container.classList.add('light-theme');
     }
-
+    static redTheme(container){
+        container.classList.remove('dark-theme', 'light-theme');
+        container.classList.add('red-theme');
+    }
 }
